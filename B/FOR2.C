3ae73a5171523e2dcef3e733970cf6710e0285a8
Pedro de Medeiros <pedro.medeiros@gmail.com>
2022-07-10 01:41:09 -0300

    update coordinates

--- B/FOR2.C
+++ B/FOR2.C
@@ -1,3 +1,53 @@
+int8_t xdir2;
+int8_t ydir2;
+
+
+/* Calculate absolute position of screen */
+void calc_xy_abs()
+{
+    scr_x_cr = scr_x;
+    scr_x += xdir2;
+    // if (xdir2 & 0b1000000) {
+    //     scr_x += xdir2
+    // } else {
+    //     scr_x -= xdir2
+    // }
+
+    scr_y_cr = scr_y;
+    scr_x += xdir2;
+    // if (ydir2 & 0b1000000) {
+    //     scr_y += ydir2;
+    // } else {
+    //     scr_y -= ydir2;
+    // }
+}
+
+
+/* */
+inline void calc_x_rel()
+{
+    if (!timer) {
+        // detect change direction?
+        if (tmp_x_pos & 0b1000000) {
+            tmp_x_pos &= 0xf;
+            offset_x = tmp_x_pos;
+            x_pos = tmp_x_pos;
+            goto y;
+        }
+    }
+    x_pos = (scr_x + offset_x) & 0xf;
+y:
+    if (!timer) {
+        // detect change direction?
+        if (tmp_y_pos & 0b1000000) {
+            tmp_y_pos &= 0xf;
+            offset_y = tmp_y_pos;
+            y_pos = tmp_y_pos;
+        }
+    }
+    y_pos = (scr_y + offset_y) & 0xf;
+}
+
 
 int main()
 {
@@ -36,9 +86,10 @@ int main()
 
     // main loop
     do {
+        calc_xy_abs();
+        calc_xy_rel();
 
-        
         split_screen()
-        
+
     while (1);
 }
